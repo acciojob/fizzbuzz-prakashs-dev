1f494f6af15fcf2525622744dafbd19baf719e5a
Prakash S <79446554+prakashs-dev@users.noreply.github.com>
2023-11-16 13:02:51 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 //your JS code here. If required.
+function fizz() {
+  for (let i = 1; i <= 20; i++) {
+    if (i % 3 == 0 && i % 5 != 0) {
+      alert("\nFizz");
+    } else if (i % 5 == 0 && i % 3 != 0) {
+      alert("\nBuzz");
+    } else if (i % 3 == 0 && i % 5 == 0) {
+      alert("\nFizzBuzz");
+    }
+  }
+}
+fizz();
